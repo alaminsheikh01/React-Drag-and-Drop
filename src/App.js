de14f12a7255ad2a0d14143525f01c63478d8afa
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-09-20 13:06:43 +0600

    drag and drop done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import DragDrop from "./components/DragDrop";
+import "./App.css";
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <DragDrop />
-    </DndProvider>
+    <div className="App">
+      <DndProvider backend={HTML5Backend}>
+        <DragDrop />
+      </DndProvider>
+    </div>
   );
 }
 
--- src/components/DragDrop.jsx
+++ src/components/DragDrop.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Picture from "./Picture";
 import "../App.css";
+import { useDrop } from "react-dnd";
+
+const PicuteList = [
+  {
+    id: 1,
+    url: "https://source.unsplash.com/random/200x200?sig=3",
+  },
+  {
+    id: 2,
+    url: "https://source.unsplash.com/random/200x200?sig=2",
+  },
+  {
+    id: 3,
+    url: "https://source.unsplash.com/random/200x200?sig=1",
+  },
+];
 
 const DragDrop = () => {
-  const PicuteList = [
-    {
-      id: 1,
-      url: "https://source.unsplash.com/random/200x200?sig=3",
-    },
-    {
-      id: 2,
-      url: "https://source.unsplash.com/random/200x200?sig=2",
-    },
-    {
-      id: 3,
-      url: "https://source.unsplash.com/random/200x200?sig=1",
-    },
-  ];
+  const [board, setBoard] = useState([]);
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "image",
+    drop: (item) => addImageToBoard(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addImageToBoard = (id) => {
+    const pictureList = PicuteList.filter((picture) => id === picture.id);
+    // setBoard((board) => [...board, pictureList[0]]);
+    setBoard([pictureList[0]]);
+  };
 
   return (
     <>
@@ -25,7 +41,11 @@ const DragDrop = () => {
           <Picture url={picture.url} id={picture.id} />
         ))}
       </div>
-      <div className="Board"></div>
+      <div className="Board" ref={drop}>
+        {board.map((picture) => (
+          <Picture url={picture.url} id={picture.id} />
+        ))}
+      </div>
     </>
   );
 };
